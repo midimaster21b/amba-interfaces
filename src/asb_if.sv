b22fb8476b570d09e2bd52051467b95aeab96c9c
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-11-05 21:53:19 -0400

    Initial ASB interface

---
+++ src/asb_if.sv
@@ -0,0 +1,18 @@
+interface asb_if;
+   logic agntx;
+   logic areqx;
+   logic [31:0] ba;
+   logic	bclk;
+   logic [31:0] bd;
+   logic	berror;
+   logic	blast;
+   logic	blok;
+   logic	bnres;
+   logic [1:0]	bprot;
+   logic [1:0]	bsize;
+   logic [1:0]	btran;
+   logic	bwait;
+   logic	bwrite;
+   logic	dselx;
+
+endinterface // asb_if
